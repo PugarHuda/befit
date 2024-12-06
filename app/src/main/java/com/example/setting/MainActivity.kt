24bb061f3fc3fcf2b51afd298353d800098d0320
raisha31 <22523110@students.uii.ac.id>
2024-12-06 21:47:40 +0700

    setting

--- app/src/main/java/com/example/setting/MainActivity.kt
+++ app/src/main/java/com/example/setting/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.setting
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }*/
+//        KE PAGE EDIT PROFILE
+        findViewById<Button>(R.id.EditProf).setOnClickListener {
+            val intent = Intent(this, EditProfile::class.java)
+            startActivity(intent)
         }
+
+//        KE PAGE EDIT PASS
+        findViewById<Button>(R.id.ChangePass).setOnClickListener {
+            val intent = Intent(this, EditPass::class.java)
+            startActivity(intent)
+        }
+
+
     }
 }
