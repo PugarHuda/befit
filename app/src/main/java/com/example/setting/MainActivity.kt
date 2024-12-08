1a6d1f69ce2df08e10e21da4ef1ad0d8f4219fd5
raisha31 <22523110@students.uii.ac.id>
2024-12-08 19:17:17 +0700

    popup

--- app/src/main/java/com/example/setting/MainActivity.kt
+++ app/src/main/java/com/example/setting/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.setting
 
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+      /*  ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+
 //        KE PAGE EDIT PROFILE
         findViewById<Button>(R.id.EditProf).setOnClickListener {
             val intent = Intent(this, EditProfile::class.java)
@@ -31,6 +34,38 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // Inventory
+        findViewById<Button>(R.id.Inventory).setOnClickListener {
+            val intent = Intent(this, EditPass::class.java)
+            startActivity(intent)
+        }
 
+        // Logout
+        findViewById<Button>(R.id.Logout).setOnClickListener {
+            showDeleteAccountDialog()
+        }
+    }
+    private fun showDeleteAccountDialog() {
+        // Membuat dialog
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.logout_box)
+
+        // Menghubungkan tombol pada dialog
+        val deleteButton = dialog.findViewById<Button>(R.id.btn_delete_account)
+        val cancelButton = dialog.findViewById<Button>(R.id.btn_cancel)
+
+        // Klik Delete Account
+        deleteButton.setOnClickListener {
+            Toast.makeText(this, "Account deleted successfully", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        // Klik Cancel
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        // Menampilkan dialog
+        dialog.show()
     }
 }
